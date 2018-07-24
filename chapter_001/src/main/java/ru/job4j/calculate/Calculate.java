349ee8fca4115df401a35de75fac025089b0848d
maratimaev <mailto:imaevmarat@outlook.com>
2018-07-24 19:16:50 +0500

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author Marat Imaev (imaevmarat@outlook.com)
+*/
 public class Calculate{
-
+        /**
+	* Main.
+	* @param args - args.
+	* @return string Hello
+	*/
 	public static void main(String[] args){
-	System.out.println("Hello world!");
+		System.out.println("Hello world!");
 	}
 
 }
