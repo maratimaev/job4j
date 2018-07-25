0adeca2b01be694e802c890a9cd74d703287af36
Marat Imaev <mailto:imaevmarat@outlook.com>
2018-07-25 13:21:06 +0300

    1.4.JUnit

--- job4j/chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ job4j/chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -16,11 +16,12 @@ public class Calculate{
 	}
 
 	/**
+	/*
 	* Method echo.
 	* @param name Your name.
 	* @return Echo plus your name.
 	*/
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
-	}
+	} 
 }
