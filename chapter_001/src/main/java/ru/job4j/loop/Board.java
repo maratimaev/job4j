a22b25ccffe873232199a0deb7593d2cb39167b0
maratimaev <mailto:imaevmarat@outlook.com>
2018-07-29 20:23:55 +0500

    Добавил описание в board, переписал факториал без рекурсии

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -1,20 +1,32 @@
 package ru.job4j.loop;
 
+
+/**
+ * @author Marat Imaev (mailto:imaevmarat@outlook.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Board {
+
+    /**
+     * Возвращает строку - шахматную доску.
+     * @param width - ширина доски.
+     * @param height - высота доски.
+     * @return строка с разделителями.
+     */
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                // условие проверки, что писать пробел или X
-                // Выше в задании мы определили закономерность, когда нужно проставлять X
+
                 if (((i + j) & 1) == 0) {
                     screen.append("X");
                 } else {
                     screen.append(" ");
                 }
             }
-            // добавляем перевод на новую строку.
+
             screen.append(ln);
         }
         return screen.toString();
--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,15 +1,28 @@
 package ru.job4j.loop;
 
+/**
+ * @author Marat Imaev (mailto:imaevmarat@outlook.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Factorial {
-
-
+    /**
+     * Возвращает факториал n.
+     * @param n - чило для расчета факториала.
+     * @return факториал.
+     */
     public int calc(int n) {
-        int fact;
+        int fact = 1;
         if (n == 0 || n == 1) {
             return 1;
         }
 
-        fact = calc(n - 1) * n;
+        for (int i = n; i >= 2; i--) {
+            fact *= i;
+        }
         return fact;
+
+//        fact = calc(n - 1) * n; вариант с рекурсией
+
     }
 }
