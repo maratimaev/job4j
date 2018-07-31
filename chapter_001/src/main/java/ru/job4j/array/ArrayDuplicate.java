9e1156c13fb7a5a7c15e5b07e8512301a555fb0f
Marat Imaev <mailto:imaevmarat@outlook.com>
2018-07-31 14:39:46 +0300

    6.8. Удаление дубликатов в массиве. [#225]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -1,7 +1,19 @@
-package ru.job4j.array;
+    package ru.job4j.array;
 
-public class ArrayDuplicate {
-//    public String[] remove(String[] array) {
+    import java.util.Arrays;
 
-//    }
-}
+    public class ArrayDuplicate {
+        public String[] remove(String[] array) {
+            int arrend = array.length;
+            for (int i = 0; i < arrend; i++) {
+                for (int j = i + 1; j < arrend; j++) {
+                    if (array[i].equals(array[j])) {
+                        array[j] = array[arrend - 1];
+                        arrend--;
+                        j--;
+                    }
+                }
+            }
+            return Arrays.copyOf(array, arrend);
+        }
+    }
--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -1,4 +1,17 @@
 package ru.job4j.array;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        String[] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+        String[] output = {"Привет", "Мир", "Супер"};
+        ArrayDuplicate str = new ArrayDuplicate();
+        String[] result = str.remove(input);
+        assertThat(result, is(output));
+    }
 }
