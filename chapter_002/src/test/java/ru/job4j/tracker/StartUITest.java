4a623bf81f1592837831f02e0bf6d4bfb8b677ed
Marat Imaev <mailto:imaevmarat@outlook.com>
2018-08-08 15:27:29 +0500

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя [#785][#65347] добавил тесты

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -21,4 +21,23 @@ public class StartUITest {
         new StartUI(input, tracker).init();
         assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
     }
+    @Test
+    public void whenDeleteThenTrackerHasDeleteValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item1 = tracker.add(new Item("test name1", "desc1"));
+        Input input = new StubInput(new String[]{"3", item.getId(), "yes", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.getAll()[0].getName(), is("test name1"));
+    }
+    @Test
+    public void whenFindByNameThenTrackerReturnArrayOfFindItems() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item1 = tracker.add(new Item("test name1", "desc1"));
+        Item item2 = tracker.add(new Item("test name", "desc2"));
+        Input input = new StubInput(new String[]{"5", item.getName(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.getAll()[1].getDescription(), is("desc2"));
+    }
 }
