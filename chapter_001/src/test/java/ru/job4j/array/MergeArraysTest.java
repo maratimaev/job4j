04253668bcf1f462958826b4dc44c50621fe2038
Marat Imaev <mailto:imaevmarat@outlook.com>
2018-08-08 13:43:37 +0500

    Слияние 2х отсортированных массивов[#64999] добавил тесты

--- chapter_001/src/test/java/ru/job4j/array/MergeArraysTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MergeArraysTest.java
@@ -14,4 +14,40 @@ public class MergeArraysTest {
         int[] result = arr.mergeArr(a, b);
         assertThat(result, is(c));
     }
+    @Test
+    public void mergeWhenArrayASmaller() {
+        int[] a = {1, 3, 5};
+        int[] b = {6, 7, 9};
+        int[] c = {1, 3, 5, 6, 7, 9};
+        MergeArrays arr = new MergeArrays();
+        int[] result = arr.mergeArr(a, b);
+        assertThat(result, is(c));
+    }
+    @Test
+    public void mergeWhenArrayBSmaller() {
+        int[] a = {6, 7, 9};
+        int[] b = {1, 3, 5};
+        int[] c = {1, 3, 5, 6, 7, 9};
+        MergeArrays arr = new MergeArrays();
+        int[] result = arr.mergeArr(a, b);
+        assertThat(result, is(c));
+    }
+    @Test
+    public void mergeWhenArrayAEmpty() {
+        int[] a = {};
+        int[] b = {1, 3, 5};
+        int[] c = {1, 3, 5};
+        MergeArrays arr = new MergeArrays();
+        int[] result = arr.mergeArr(a, b);
+        assertThat(result, is(c));
+    }
+    @Test
+    public void mergeWhenArrayBEmpty() {
+        int[] a = {1, 3, 5};
+        int[] b = {};
+        int[] c = {1, 3, 5};
+        MergeArrays arr = new MergeArrays();
+        int[] result = arr.mergeArr(a, b);
+        assertThat(result, is(c));
+    }
 }
