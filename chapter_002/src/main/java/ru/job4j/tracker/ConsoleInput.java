c7053f4b509d26f462b66305b76f0c402f3e17f0
Marat Imaev <mailto:imaevmarat@outlook.com>
2018-08-08 10:27:44 +0500

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя [#785]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
 
+    @Override
     public String ask(String question) {
         System.out.println(question);
         return scanner.nextLine();
---
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class StartUITest {
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.getAll()[0].getName(), is("test name"));
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
+    }
+}
