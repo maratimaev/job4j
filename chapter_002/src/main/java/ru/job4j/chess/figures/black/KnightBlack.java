959f7f8bec0c759e28a88429021d95de8b8edbfa
Marat Imaev <mailto:imaevmarat@outlook.com>
2018-08-15 17:20:09 +0500

    Каркас шахматной доски [#792]

---
+++ chapter_002/src/main/java/ru/job4j/chess/figures/black/KnightBlack.java
@@ -0,0 +1,32 @@
+package ru.job4j.chess.figures.black;
+
+import ru.job4j.chess.figures.*;
+
+/**
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class KnightBlack implements Figure {
+    private final Cell position;
+
+    public KnightBlack(final Cell position) {
+        this.position = position;
+    }
+
+    @Override
+    public Cell position() {
+        return this.position;
+    }
+
+    @Override
+    public Cell[] way(Cell source, Cell dest) {
+        return new Cell[] { dest };
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new KnightBlack(dest);
+    }
+}
