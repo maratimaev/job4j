7105d907234a1e8fff9004ff1a98d1af665f472a
Marat Imaev <mailto:imaevmarat@outlook.com>
2018-08-17 16:30:48 +0500

    Каркас шахматной доски [#792] промежуточный результат для слона

--- chapter_002/src/main/java/ru/job4j/chess/Logic.java
+++ chapter_002/src/main/java/ru/job4j/chess/Logic.java
@@ -1,10 +1,8 @@
 package ru.job4j.chess;
 
 import ru.job4j.chess.figures.*;
-import java.util.Optional;
 
 /**
-  *
  * @author Petr Arsentev (parsentev@yandex.ru)
  * @version $Id$
  * @since 0.1
@@ -19,14 +17,28 @@ public class Logic {
 
     public boolean move(Cell source, Cell dest) {
         boolean rst = false;
-        int index = this.findBy(source);  //находим номер в массиве figures
-        if (index != -1) {
+        try {
+            int index = this.findBy(source);  //находим номер в массиве figures
             Cell[] steps = this.figures[index].way(source, dest);
-
-            if (steps[0] != null && steps[steps.length - 1].equals(dest)) {
-                rst = true;
-                this.figures[index] = this.figures[index].copy(dest);
+            if (!steps[steps.length - 1].equals(dest)) {
+                throw new ImposibleMoveException("Ошибка движения");
             }
+            for (int i = 0; i < steps.length; i++) {
+                for (Figure figure : figures) {
+                    if (figure.position().equals(steps[i])) {
+                        throw new OccupiedWayException("Клетки заняты");
+                    }
+                }
+            }
+            rst = true;
+            this.figures[index] = this.figures[index].copy(dest);
+
+        } catch (ImposibleMoveException ime) {
+            // todo
+        } catch (OccupiedWayException owe) {
+            // todo
+        } catch (FigureNotFoundException fnfe) {
+            // todo
         }
         return rst;
     }
@@ -45,6 +57,9 @@ public class Logic {
                 rst = index;
                 break;
             }
+        }
+        if (rst == -1) {
+            throw new FigureNotFoundException("Фигура не найдена");
         }
         return rst;
     }
--- chapter_002/src/main/java/ru/job4j/chess/figures/black/BishopBlack.java
+++ chapter_002/src/main/java/ru/job4j/chess/figures/black/BishopBlack.java
@@ -2,13 +2,13 @@ package ru.job4j.chess.figures.black;
 
 import ru.job4j.chess.figures.Cell;
 import ru.job4j.chess.figures.Figure;
+import ru.job4j.chess.figures.ImposibleMoveException;
 
 import java.util.Arrays;
 
 import static java.lang.Math.abs;
 
 /**
- *
  * @author Petr Arsentev (parsentev@yandex.ru)
  * @version $Id$
  * @since 0.1
@@ -29,12 +29,13 @@ public class BishopBlack implements Figure {
     public Cell[] way(Cell source, Cell dest) {
         Cell[] steps = new Cell[8];
         int deltaX, deltaY;
-        if (abs(dest.x - source.x) == abs(dest.y - source.y)) {
-            deltaX = (dest.x - source.x > 0) ? 1 : -1;
-            deltaY = (dest.y - source.y > 0) ? 1 : -1;
-            for (int i = 0; i < abs(dest.x - source.x); i++) {
-                steps[i] = Cell.values()[8 * (source.x + deltaX * (i + 1)) + (source.y + deltaY * (i + 1))];
-            }
+        if (abs(dest.x - source.x) != abs(dest.y - source.y)) {
+            throw new ImposibleMoveException("Слон так не ходит");
+        }
+        deltaY = (dest.y - source.y > 0) ? 1 : -1;
+        deltaX = (dest.x - source.x > 0) ? 1 : -1;
+        for (int i = 0; i < abs(dest.x - source.x); i++) {
+            steps[i] = Cell.values()[8 * (source.x + deltaX * (i + 1)) + (source.y + deltaY * (i + 1))];
         }
         return Arrays.copyOf(steps, abs(dest.x - source.x));
     }
