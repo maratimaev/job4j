6c40ea0dbae504f92835656898e86bb0ae079ba7
maratimaev <mailto:imaevmarat@outlook.com>
2018-08-21 19:14:05 +0500

    Сделал way для короля и пешки

--- chapter_002/src/main/java/ru/job4j/chess/figures/black/KingBlack.java
+++ chapter_002/src/main/java/ru/job4j/chess/figures/black/KingBlack.java
@@ -2,6 +2,10 @@ package ru.job4j.chess.figures.black;
 
 import ru.job4j.chess.figures.*;
 
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+
 /**
  *
  * @author Petr Arsentev (parsentev@yandex.ru)
@@ -10,11 +14,9 @@ import ru.job4j.chess.figures.*;
  */
 public class KingBlack implements Figure {
     private final Cell position;
-
     public KingBlack(final Cell position) {
         this.position = position;
     }
-
     @Override
     public Cell position() {
         return this.position;
@@ -22,9 +24,19 @@ public class KingBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        return new Cell[] {
-                dest
-        };
+        Cell[] steps = new Cell[1];
+        int deltaX, deltaY;
+        int absDeltaX = abs(dest.x - source.x);
+        int absDeltaY = abs(dest.y - source.y);
+        if (absDeltaX > 1 || absDeltaY > 1) {
+            throw new ImposibleMoveException("Король так не ходит");
+        }
+        deltaY = dest.y - source.y;
+        deltaY = (deltaY > 0) ? 1 : ((deltaY < 0) ? -1 : deltaY);
+        deltaX = dest.x - source.x;
+        deltaX = (deltaX > 0) ? 1 : ((deltaX < 0) ? -1 : deltaX);
+        steps[0] = Cell.values()[8 * (source.x + deltaX) + (source.y + deltaY)];
+        return Arrays.copyOf(steps, (absDeltaX + absDeltaY));
     }
 
     @Override
--- chapter_002/src/main/java/ru/job4j/chess/figures/black/PawnBlack.java
+++ chapter_002/src/main/java/ru/job4j/chess/figures/black/PawnBlack.java
@@ -2,6 +2,10 @@ package ru.job4j.chess.figures.black;
 
 import ru.job4j.chess.figures.*;
 
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+
 /**
  *
  * @author Petr Arsentev (parsentev@yandex.ru)
@@ -22,13 +26,18 @@ public class PawnBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        Cell[] steps = new Cell[0];
-        if (((source.y == 6 && source.y == dest.y + 2) || (source.y == dest.y + 1)) && source.x == dest.x) {
-            steps = new Cell[] {
-                    dest
-            };
+        Cell[] steps = new Cell[2];
+        int deltaX, deltaY;
+        int absDeltaY = abs(dest.y - source.y);
+        if (!((dest.x == source.x) & ((source.y == dest.y + 1) || ((source.y == 6) & (source.y == dest.y + 2))))) {
+            throw new ImposibleMoveException("Пешка так не ходит");
         }
-        return steps;
+        deltaY = -1;
+        deltaX = 0;
+        for (int i = 0; i < absDeltaY; i++) {
+            steps[i] = Cell.values()[8 * (source.x + deltaX * (i + 1)) + (source.y + deltaY * (i + 1))];
+        }
+        return Arrays.copyOf(steps, absDeltaY);
     }
 
     @Override
