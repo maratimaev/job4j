1c487553a42339583454995e8371394365de0fa0
maratimaev <mailto:imaevmarat@outlook.com>
2018-08-29 22:00:15 +0500

    3. Компаратор для строк. [#35008]

--- chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -1,14 +1,30 @@
 package ru.job4j.comparator;
 
+/**
+ * @author Marat Imaev (mailto:imaevmarat@outlook.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class ListCompare {
 
+    /**
+     * Метод сравнивает 2 строки посимвольно
+     * @param s1 типа String
+     * @param s2 типа String
+     * @return int
+     */
     public int compare(String s1, String s2) {
-        for
-        return 0;
+        int result = 0;
+        for (int i = 0; i < Math.max(s1.length(), s2.length()); i++) {
+            result = Character.compare(s1.charAt(i), s2.charAt(i));
+            if (result != 0) {
+                break;
+            }
+            if ((i == s2.length() - 1) || (i == s1.length() - 1)) {
+                result = s1.length() - s2.length();
+                break;
+            }
+        }
+        return result;
     }
-//    String.charAt(int index)
-//
-//            Integer.compare(int left, int right),
-//
-//            Character.compare(char left, char right);
 }
--- chapter_003/src/test/java/ru/job4j/comparator/ListCompareTest.java
+++ chapter_003/src/test/java/ru/job4j/comparator/ListCompareTest.java
@@ -6,9 +6,17 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+/**
+ * @author Marat Imaev (mailto:imaevmarat@outlook.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class ListCompareTest {
+    /**
+     * Тест проверяет сравнение 2х одинаковых строк
+     */
     @Test
-    public void whenStringsAreEqualThenZero () {
+    public void whenStringsAreEqualThenZero() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Ivanov",
@@ -17,8 +25,11 @@ public class ListCompareTest {
         assertThat(rst, is(0));
     }
 
+    /**
+     * Тест проверяет сравнение разных по длине строк
+     */
     @Test
-    public void whenLeftLessThanRightResultShouldBeNegative () {
+    public void whenLeftLessThanRightResultShouldBeNegative() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Ivanov",
@@ -27,8 +38,11 @@ public class ListCompareTest {
         assertThat(rst, lessThan(0));
     }
 
+    /**
+     * Тест проверяет сравнение разных по длине и содержанию строк
+     */
     @Test
-    public void whenLeftGreaterThanRightResultShouldBePositive () {
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Petrov",
@@ -37,8 +51,11 @@ public class ListCompareTest {
         assertThat(rst, greaterThan(0));
     }
 
+    /**
+     * Тест проверяет сравнение одинаковых по длине с одной отличающейся буквой
+     */
     @Test
-    public void secondCharOfLeftGreaterThanRightShouldBePositive(){
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Petrov",
@@ -47,8 +64,11 @@ public class ListCompareTest {
         assertThat(rst, greaterThan(0));
     }
 
+    /**
+     * Тест проверяет сравнение когда 1ая строка длинее второй
+     */
     @Test
-    public void secondCharOfLeftLessThanRightShouldBeNegative(){
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Patrova",
