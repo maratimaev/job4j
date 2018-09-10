a2ec71fa8af737dddb1b6e82451a8d6ad2de8231
Marat Imaev <mailto:imaevmarat@outlook.com>
2018-09-10 16:05:52 +0500

    Отсортировать департаменты [#34352][#70287]

--- chapter_004/src/main/java/ru/job4j/departments/Departments.java
+++ chapter_004/src/main/java/ru/job4j/departments/Departments.java
@@ -1,22 +1,42 @@
 package ru.job4j.departments;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.TreeSet;
 
+/**
+ * @author Marat Imaev (mailto:imaevmarat@outlook.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Departments {
+    /** Поле упорядоченный Set подразделений */
+    private TreeSet<String> result;
+
+    public Departments(TreeSet<String> result) {
+        this.result = result;
+    }
+
+    /**
+     * Метод сравнивает 2 строки посимвольно
+     * @param strArray типа String[]
+     * @return отсортированный массив, с добавлением отсутствующих подразделений String[]
+     */
     public String[] sort(String[] strArray) {
-        ArrayList<String> rsltArray = new ArrayList<>();
         for (String str: strArray) {
-            String[] splited = str.split("\\\\");
             StringBuilder k = new StringBuilder();
-            for (String s: splited) {
-                if (!rsltArray.contains(k + s)) {
-                    rsltArray.add(k + s);
-                }
+            for (String s: splitDepartments(str)) {
+                result.add(k + s);
                 k.append(s).append("\\");
             }
         }
-        Collections.sort(rsltArray);
-        return rsltArray.toArray(new String[0]);
+        return result.toArray(new String[0]);
+    }
+
+    /**
+     * Метод разделяет строки на подразделения
+     * @param str типа String
+     * @return массив подразделений String[]
+     */
+    private String[] splitDepartments(String str) {
+        return str.split("\\\\");
     }
 }
--- chapter_004/src/test/java/ru/job4j/departments/DepartmentsTest.java
+++ chapter_004/src/test/java/ru/job4j/departments/DepartmentsTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.departments;
 
 import org.junit.Test;
+import java.util.TreeSet;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,7 +12,7 @@ import static org.junit.Assert.assertThat;
  */
 public class DepartmentsTest {
     /**
-     * Тест проверяет вычисление линейной функции
+     * Тест проверяет сортировку департаментов
      */
     @Test
     public void sortDepartmentsTest() {
@@ -35,7 +36,45 @@ public class DepartmentsTest {
                 "K2\\SK1\\SSK1",
                 "K2\\SK1\\SSK2"
         };
-        Departments departments = new Departments();
+        Departments departments = new Departments(new TreeSet<>());
+        assertThat(departments.sort(source), is(sorted));
+    }
+    /**
+     * Тест проверяет сортировку департаментов в обратном порядке
+     */
+    @Test
+    public void sortReverseDepartmentsTest() {
+        String[] source = {
+                "K1\\SK1",
+                "K2\\SK1\\SSK1",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K2",
+                "K1\\SK2",
+                "K2\\SK1\\SSK2"
+        };
+        String[] sorted = {
+                "K2\\SK1\\SSK2",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1",
+                "K2",
+                "K1\\SK2",
+                "K1\\SK1\\SSK2",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1",
+                "K1"
+        };
+        Departments departments = new Departments(new TreeSet<>(new ReverseDepartments()));
+        assertThat(departments.sort(source), is(sorted));
+    }
+    /**
+     * Тест проверяет сортировку пустого списка
+     */
+    @Test
+    public void sortEmptyDepartmentsTest() {
+        String[] source = {};
+        String[] sorted = {};
+        Departments departments = new Departments(new TreeSet<>());
         assertThat(departments.sort(source), is(sorted));
     }
 }
